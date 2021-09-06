d87b8f5a69db470dc8dcd963d601cea0b2a8e1ba
Laurentiu Ciobanu <ciobanulaur@gmail.com>
2021-09-06 06:13:02 +0000

    fix: create output dir if it does not exist

--- src/output/index.ts
+++ src/output/index.ts
@@ -3,6 +3,7 @@ import {writeFile} from "fs/promises";
 import * as fs from "fs";
 import {dirname} from "path";
 import {OutputTransformer} from "./transformer";
+import {log} from "..";
 
 export * from "./transformer";
 
@@ -23,7 +24,8 @@ const output = {
     async bundleToDisk(path: string, options?: OutputOptions) {
         const bundle = getOutputAsFile(options?.source ?? cache, options?.transformers ?? []);
 
-        if (fs.existsSync(dirname(path))) {
+        if (!fs.existsSync(dirname(path))) {
+            log.debug(`creating directory ${dirname(path)}`);
             fs.mkdirSync(dirname(path), {recursive: true});
         }
 
